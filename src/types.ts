efba8f333dbdb2632331c11c79620a61e7f39941
Nick Payne <nick@kurai.co.uk>
2025-07-27 18:45:29 +0100

    initial wrapper

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export interface AudioTeeOptions {
+  sampleRate?: number
+  chunkDuration?: number
+  mute?: boolean
+  includeProcesses?: number[]
+  excludeProcesses?: number[]
+}
+
+export interface AudioChunk {
+  data: Buffer
+}
+
+export type LogLevel = 'metadata' | 'stream_start' | 'stream_stop' | 'info' | 'error' | 'debug'
+
+export interface LogMessage {
+  timestamp: Date
+  message_type: LogLevel
+  data: {
+    message: string
+  }
+}
+
+export interface AudioTeeEvents {
+  data: (chunk: AudioChunk) => void
+  start: () => void
+  stop: () => void
+  error: (error: Error) => void
+  log: (message: string, level: LogLevel) => void
+}
